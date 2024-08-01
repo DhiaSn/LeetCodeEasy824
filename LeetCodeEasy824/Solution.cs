eb60209fd47c940352df4c207a28fb43788b2914
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2024-08-01 14:54:32 +0100

    optimize the code

--- LeetCodeEasy824/Solution.cs
+++ LeetCodeEasy824/Solution.cs
@@ -8,18 +8,16 @@
 
             for (int i = 0; i < words.Length; i++)
             {
-                char firstChar = words[i].ToLower()[0];
-
-                if (firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o' || firstChar == 'u')
+                if ("aeiou".Contains(words[i].ToLower()[0]))
                     words[i] = $"{words[i]}ma";
                 else
                 {
-                    char temp = words[i][0]; 
+                    char temp = words[i][0];
                     words[i] = words[i].Remove(0, 1);
                     words[i] = $"{words[i]}{temp}ma";
                 }
 
-                for (int j = 0; j < i +1; j++)
+                for (int j = 0; j < i + 1; j++)
                     words[i] += "a";
             }
             return string.Join(' ', words);
