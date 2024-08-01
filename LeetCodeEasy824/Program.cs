0285fa77ed12794b495256bec9cc4aaebba27fdc
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2024-08-01 14:40:49 +0100

    Add project files.

---
+++ LeetCodeEasy824/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeEasy824
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result1 = Solution.ToGoatLatin("I speak Goat Latin");
+            if (result1 != "Imaa peaksmaaa oatGmaaaa atinLmaaaaa")
+            {
+                Console.WriteLine($"result1 is: {result1} instead of \"Imaa peaksmaaa oatGmaaaa atinLmaaaaa\"");
+                return;
+            }
+            string result2 = Solution.ToGoatLatin("The quick brown fox jumped over the lazy dog");
+            if (result2 != "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa")
+            {
+                Console.WriteLine($"result2 is: {result2} instead of \"heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa\"");
+                return;
+            }
+            
+            Console.WriteLine($"it works perfectly!");
+        }
+    }
+}
---
+++ LeetCodeEasy824/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeEasy824
+{
+    public static class Solution
+    {
+        public static string ToGoatLatin(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                char firstChar = words[i].ToLower()[0];
+
+                if (firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o' || firstChar == 'u')
+                    words[i] = $"{words[i]}ma";
+                else
+                {
+                    char temp = words[i][0]; 
+                    words[i] = words[i].Remove(0, 1);
+                    words[i] = $"{words[i]}{temp}ma";
+                }
+
+                for (int j = 0; j < i +1; j++)
+                    words[i] += "a";
+            }
+            return string.Join(' ', words);
+        }
+    }
+}
